83a96a2249ef0dd08251f17679e6232a599892f2
Yihui Xie <xie@yihui.name>
2014-12-29 15:49:40 -0600

    a first sketch of the package

---
+++ R/hello.R
@@ -0,0 +1,11 @@
+#' A hello function
+#'
+#' Prints \code{Hello World!}.
+#'
+#' This function was written in R/hello.R, and all other functions in this
+#' package were written in R Markdown documents.
+#' @export
+#' @examples rlp::hello()
+hello = function() {
+  cat("Hello, world!\n")
+}
